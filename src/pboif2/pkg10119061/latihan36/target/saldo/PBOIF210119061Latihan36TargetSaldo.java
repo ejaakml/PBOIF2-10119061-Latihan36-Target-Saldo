768798d5b6846b0334640e2c78b96006f3cbc3fe
ejaakml <73114808+ejaakml@users.noreply.github.com>
2020-11-07 20:12:41 +0700

    Update PBOIF210119061Latihan36TargetSaldo.java

--- src/pboif2/pkg10119061/latihan36/target/saldo/PBOIF210119061Latihan36TargetSaldo.java
+++ src/pboif2/pkg10119061/latihan36/target/saldo/PBOIF210119061Latihan36TargetSaldo.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  * NAMA  : RHEIZA AKMAL R
  * KELAS : IF2
  * NIM   : 10119061
+ * Deskripsi Program : untuk menghitung target saldo 
  */
 public class PBOIF210119061Latihan36TargetSaldo {
 
